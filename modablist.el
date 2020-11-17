238f81bb00041b8e2b8c36bcc637f630ae9e974c
Jen-Chieh <jcs090218@gmail.com>
2020-11-17 13:14:22 +0800

    Add key map.

--- modablist.el
+++ modablist.el
@@ -40,5 +40,41 @@
   :group 'tool
   :link '(url-link :tag "Repository" "https://github.com/jcs-elpa/modablist"))
 
+(defconst modablist--buffer-name-format "*modablist:<%s>*"
+  "String format for buffer name.")
+
+(defvar modablist--buffer-index 0
+  "Global buffer name index.
+
+This value will be increment by 1 for every new modablist creation.")
+
+(defvar-local modablist-data '()
+  "Table data in list.")
+
+(defun modablist--change-data (row column value)
+  ""
+  (setf (nth column (nth row modablist-data)) value))
+
+(defvar modablist-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "<return>") #'npm-pkgs-unmark-mark)
+    map)
+  "Kaymap for `modablist-mode'.")
+
+(define-derived-mode modablist-mode tabulated-list-mode
+  "modablist-mode"
+  "Major mode for `modablist'."
+  :group 'modablist
+  (easy-tabulated-list-make modablist-data :padding 1))
+
+;;;###autoload
+(defun modablist-new ()
+  "Create a new modablist data."
+  (interactive)
+  (let ((name (format modablist--buffer-name-format modablist--buffer-index)))
+    (pop-to-buffer name nil)
+    (modablist-mode))
+  (setq modablist--buffer-index (1+ modablist--buffer-index)))
+
 (provide 'modablist)
 ;;; modablist.el ends here
