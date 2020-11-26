6e4542d8328ff824c463ab2e0580cc55254b67a8
Jen-Chieh <jcs090218@gmail.com>
2020-11-26 15:12:45 +0800

    Update core with selection.

--- modablist.el
+++ modablist.el
@@ -7,7 +7,7 @@
 ;; Description: Modifiable tabulated-list extension.
 ;; Keyword: table list tablist
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.3") (easy-tabulated-list "0.1.2")
+;; Package-Requires: ((emacs "24.3") (easy-tabulated-list "0.1.2"))
 ;; URL: https://github.com/jcs-elpa/modablist
 
 ;; This file is NOT part of GNU Emacs.
@@ -40,63 +40,68 @@
   :group 'tool
   :link '(url-link :tag "Repository" "https://github.com/jcs-elpa/modablist"))
 
-(defcustom modablist-default-width 8
-  "Default width for each column."
-  :type 'integer
-  :group 'modablist)
-
-(defcustom modablist-default-columns 26
-  "Default column when generate a new sheet."
-  :type 'integer
+(defcustom modablist-highlight-delay 0.2
+  "Seconds to delay highlight the current selection in the table."
+  :type 'float
   :group 'modablist)
 
 (defvar modablist-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "<return>") #'modablist--confirm)
     map)
-  "Kaymap for `modablist-mode'.")
+  "Kaymap for function `modablist-mode'.")
 
-(defvar modablist--tablist-format (modablist--init-tablist-format)
-  "Format to assign to `tabulated-list-format' variable.")
+(defvar modablist-mode-insert-map
+  (let ((map (make-sparse-keymap)))
+    (let ((c ?\s))
+      (while (< c ?\d)
+        (define-key map (vector c) #'self-insert-command)
+        (setq c (1+ c)))
+      (when (eq system-type 'ms-dos)
+        (setq c 128)
+        (while (< c 160)
+          (define-key map (vector c) #'self-insert-command)
+          (setq c (1+ c))))
+      (setq c 160)
+      (while (< c 256)
+        (define-key map (vector c) #'self-insert-command)
+        (setq c (1+ c))))
+    (define-key map (kbd "<return>") #'modablist--confirm)
+    map)
+  "Kaymap for function `modablist-mode' when inserting text.")
 
-(defconst modablist--buffer-name-format "*modablist:<%s>*"
-  "String format for buffer name.")
-
-(defvar modablist--buffer-index 0
-  "Global buffer name index.
-
-This value will be increment by 1 for every new modablist creation.")
-
-(defvar-local modablist-data '()
-  "Table data in list.")
+(defvar-local modablist--buffer nil
+  "Record the working buffer.")
 
 (defvar-local modablist--overlays '()
   "List of selection overlays.")
 
-(defvar-local modablist--old-key-map nil
-  "Record the old key map before editing.")
+(defvar-local modablist--column-boundary '()
+  "List fo column boundary; it uses to identify the current column in table.")
 
-(defvar-local modablist--total-rows nil
-  "Total rows in the table.")
-
-(defvar-local modablist--total-columns nil
-  "Total columns in the table")
-
-(defvar-local modablist--current-row nil "Current row the cursor current on.")
-(defvar-local modablist--current-column nil "Current column the cursor current on.")
+(defvar-local modablist--timer-selection-overlay nil
+  "Timer for selection overlay.")
 
 ;;
 ;; (@* "Util" )
 ;;
 
-;;
-;; (@* "Selection" )
-;;
+(defun modablist--in-range-p (in-val in-min in-max)
+  "Check to see if IN-VAL is between IN-MIN and IN-MAX."
+  (and (<= in-min in-val) (< in-val in-max)))
 
-(defun modablist--clear-overlays ()
-  "Remove all overlays."
-  (dolist (ov modablist--overlays) (delete-overlay ov))
-  (setq modablist--overlays '()))
+(defun modablist--column-to-pos (column)
+  "Convert COLUMN to position."
+  (save-excursion (move-to-column column) (point)))
+
+(defun modablist--kill-timer (timer)
+  "Safe way to kill the TIMER."
+  (when (timerp timer) (cancel-timer timer)))
+
+(defmacro modablist-current-buffer (&rest body)
+  "Execute BODY with in the current working buffer.."
+  (declare (indent 0) (debug t))
+  `(when modablist--buffer (with-current-buffer modablist--buffer (progn ,@body))))
 
 ;;
 ;; (@* "Table" )
@@ -105,6 +110,90 @@ This value will be increment by 1 for every new modablist creation.")
 (defun modablist--change-data (row column value)
   ""
   (setf (nth column (nth row modablist-data)) value))
+
+(defun modablist--count-rows ()
+  "Return integer value represent rows."
+  (length tabulated-list-entries))
+
+(defun modablist--count-columns ()
+  "Return integer value represent columns."
+  (length tabulated-list-format))
+
+(defun modablist--first-entry-line ()
+  "Return line number from the first entry from current buffer."
+  (save-excursion
+    (goto-char (point-min))
+    (while (and (not (tabulated-list-get-entry)) (not (eobp))) (forward-line))
+    (line-number-at-pos)))
+
+(defun modablist--current-row ()
+  "Return current row number."
+  (let ((first-entry-line (modablist--first-entry-line)))
+    (1+ (- (line-number-at-pos) first-entry-line))))
+
+(defun modablist--update-column-boundary ()
+  "Update the variable `modablist--column-boundary'."
+  (setq modablist--column-boundary '())
+  (let ((boundary tabulated-list-padding))
+    (push boundary modablist--column-boundary)
+    (mapc
+     (lambda (fmt)
+       (setq boundary (+ boundary (nth 1 fmt)))
+       (push boundary modablist--column-boundary))
+     tabulated-list-format)
+    (setq modablist--column-boundary (reverse modablist--column-boundary))))
+
+(defun modablist--current-column ()
+  "Return current column number."
+  (modablist--update-column-boundary)
+  (let ((cur-col (current-column)) (len (length modablist--column-boundary))
+        (index 0) break column upper-column lower-column)
+    (while (and (not break) (< index (1- len)))
+      (setq lower-column (nth index modablist--column-boundary)
+            upper-column (nth (1+ index) modablist--column-boundary))
+      (when (modablist--in-range-p cur-col lower-column upper-column)
+        (setq column index break t))
+      (setq index (1+ index)))
+    (when column (setq column (1+ column)))
+    column))
+
+(defun modablist--current-range ()
+  "Return current column number."
+  (modablist--update-column-boundary)
+  (let ((cur-col (current-column)) (len (length modablist--column-boundary))
+        (index 0) break column upper-column lower-column)
+    (while (and (not break) (< index (1- len)))
+      (setq lower-column (nth index modablist--column-boundary)
+            upper-column (nth (1+ index) modablist--column-boundary))
+      (when (modablist--in-range-p cur-col lower-column upper-column)
+        (setq column index break t))
+      (setq index (1+ index)))
+    (when column
+      (cons (modablist--column-to-pos lower-column)
+            (modablist--column-to-pos upper-column)))))
+
+;;
+;; (@* "Selection" )
+;;
+
+(defun modablist--make-overlay (beg end)
+  "Make selection overlay."
+  (let ((ol (make-overlay beg end)))
+    (overlay-put ol 'face 'ivy-current-match)
+    (overlay-put ol 'priority 100)
+    (push ol modablist--overlays)  ; NOTE: Eventually get managed to list.
+    ol))
+
+(defun modablist--make-selection-ov ()
+  "Make selection overlay."
+  (modablist-current-buffer modablist--buffer
+    (let ((range (modablist--current-range)))
+      (modablist--make-overlay (car range) (cdr range)))))
+
+(defun modablist--clear-overlays ()
+  "Remove all overlays."
+  (dolist (ov modablist--overlays) (delete-overlay ov))
+  (setq modablist--overlays '()))
 
 ;;
 ;; (@* "Core" )
@@ -117,60 +206,50 @@ This value will be increment by 1 for every new modablist creation.")
   (setq buffer-read-only (not buffer-read-only))
   (if buffer-read-only
       (progn
-        (setq-local modablist-mode-map modablist--old-key-map)
+        ()
         )
     )
   (message "read: %s" buffer-read-only))
 
-(defun modablist--post-command ()
-  ""
-  (message "%s" (current-column))
-  ;;(make-overlay )
-  )
-
 (defun modablist--new-row ()
-  "Generate a new row."
-  (let (new-row (cnt 0))
-    (while (< cnt modablist--total-rows) (push "" new-row) (setq cnt (1+ cnt)))
-    (push new-row modablist-data)
-    (modablist--refresh)))
+  "Create a new row."
+  (let ((cnt 0) (columns (modablist--count-columns)) new-row)
+    (while (< cnt columns) (push "" new-row) (setq cnt (1+ cnt)))
+    (setq tabulated-list-entries
+          (append tabulated-list-entries
+                  (easy-tabulated-list-form-entries new-row)))
+    (tabulated-list-revert)))
 
-(defun modablist--refresh ()
-  "Refresh `tabulated-list' once."
-  (easy-tabulated-list-make modablist--tablist-format
-                            (easy-tabulated-list-form-entries modablist-data)
-                            :padding 1)
-  (tabulated-list-revert))
+(defun modablist--post-command ()
+  "Post command for `modablist-mode'."
+  (modablist--clear-overlays)
+  (modablist--kill-timer modablist--timer-selection-overlay)
+  (setq modablist--timer-selection-overlay
+        (run-with-timer modablist-highlight-delay nil #'modablist--make-selection-ov)))
 
-(defun modablist--init-tablist-format ()
-  "Initialize the `tabulated-list' format."
-  (interactive)
-  (let* ((start (string-to-char "A")) (end (+ start modablist-default-columns)) lst)
-    (while (< start end)
-      (push (list (char-to-string start) modablist-default-width t) lst)
-      (setq start (1+ start)))
-    (vconcat (reverse lst))))
+;;
+;; (@* "Entry" )
+;;
 
-(define-derived-mode modablist-mode tabulated-list-mode
-  "modablist-mode"
-  "Major mode for `modablist'."
-  :group 'modablist
-  (setq modablist-data '())
-  (setq modablist--total-rows modablist-default-columns
-        modablist--total-columns 0
-        modablist--current-row 0
-        modablist--current-column 0)
-  (modablist--refresh)
-  (add-hook 'post-command-hook 'modablist--post-command nil t))
+(defun modablist--enable ()
+  "Enable `modablist' in current buffer."
+  (if (derived-mode-p 'tabulated-list-mode)
+      (progn
+        (setq modablist--buffer (current-buffer))
+        (add-hook 'post-command-hook #'modablist--post-command nil t))
+    (modablist-mode -1)
+    (user-error "[WARNING] You can't enable modablist in buffer that aren't derived from `tabulated-list-mode`")))
+
+(defun modablist--disable ()
+  "Disable `modablist' in current buffer."
+  (remove-hook 'post-command-hook #'modablist--post-command t))
 
 ;;;###autoload
-(defun modablist-new ()
-  "Create a new modablist data."
-  (interactive)
-  (let ((name (format modablist--buffer-name-format modablist--buffer-index)))
-    (pop-to-buffer name nil)
-    (modablist-mode))
-  (setq modablist--buffer-index (1+ modablist--buffer-index)))
+(define-minor-mode modablist-mode
+  "Minor mode 'modablist-mode'."
+  :lighter " ModLst"
+  :group modablist
+  (if modablist-mode (modablist--enable) (modablist--disable)))
 
 (provide 'modablist)
 ;;; modablist.el ends here
